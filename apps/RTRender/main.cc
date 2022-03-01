7244d8844224c59d80aab4463205f282185a4dc9
FuChan <2116642284@qq.com>
2022-03-01 22:29:44 +0800

    [Test]:add come extra test.

--- apps/RTRender/main.cc
+++ apps/RTRender/main.cc
@@ -6,6 +6,144 @@
 #include "Window/RTMessageMap.h"
 #include "Window/RTWindow.h"
 
+class Vector3D
+{
+public:
+	int x, y, z;
+};
+
+class Vertex
+{
+public:
+	Vector3D position;
+};
+
+class Triangle
+{
+public:
+	int vertexIndex[3];
+};
+
+class PixelColor
+{
+public:
+	union
+	{
+		int x;
+		unsigned char r;
+		unsigned char g;
+		unsigned char b;
+		unsigned char a;
+
+	};
+};
+
+void TriangleSample(Vertex& p1, Vertex& p2, Vertex& p3)
+{
+	float d1y = (p1.position.y - p2.position.y);
+	float d1x = (p2.position.x - p1.position.x) / d1y;
+	float d2y = (p1.position.y - p3.position.y);
+	float d2x = (p3.position.x - p1.position.x) / d2y;
+	if (p2.position.x > p3.position.x)
+	{
+		float tmp;
+		tmp = d2x;
+		d2x = d1x;
+		d1x = tmp;
+	}
+
+	int dy = d1y > 0 ? -1 : 1;
+	Vector3D m1, m2;// m1 = m2 = p1.position;
+	
+	//p1代表单独点
+	while (m1.y != p2.position.y)
+	{
+		for (int i = m1.x; i < m2.x; i++)
+		{
+			//绘制
+		}
+		m1.y += dy;
+		m1.x += d1x;
+		m2.x += d2x;
+	}
+}
+
+void Raterier(Triangle* triangles, Vertex* vertices)
+{
+	PixelColor frame[1024][648];
+	int numOfTriangle = 10;
+	Vertex* p;
+	for (int i = 0; i < numOfTriangle; i++)
+	{
+		int ay = vertices[triangles[i].vertexIndex[0]].position.y;
+		int ax = vertices[triangles[i].vertexIndex[0]].position.x;
+		int by = vertices[triangles[i].vertexIndex[1]].position.y;
+		int bx = vertices[triangles[i].vertexIndex[1]].position.x;
+		int cy = vertices[triangles[i].vertexIndex[2]].position.y;
+		int cx = vertices[triangles[i].vertexIndex[2]].position.x;
+		if ((ay - by) * (by - cy) > 0)
+		{
+			Vertex vertex;
+			p = &vertices[triangles[i].vertexIndex[1]];
+			vertex.position.x = cx + (ax - cx) * (p->position.y - cy) / (ay - cy);
+			vertex.position.y = p->position.y;
+			TriangleSample(vertices[triangles[i].vertexIndex[0]], *p, vertex);
+			TriangleSample(vertices[triangles[i].vertexIndex[2]], *p, vertex);
+		}
+		else if ((by - ay) * (ay - cy) > 0)
+		{
+			Vertex vertex;
+			p = &vertices[triangles[i].vertexIndex[0]];
+			vertex.position.x = cx + (bx - cx) * (p->position.y - cy) / (by - cy);
+			vertex.position.y = p->position.y;
+			TriangleSample(vertices[triangles[i].vertexIndex[1]], *p, vertex);
+			TriangleSample(vertices[triangles[i].vertexIndex[2]], *p, vertex);
+		}
+		else if ((ay - cy) * (cy - by) > 0)
+		{
+			Vertex vertex;
+			p = &vertices[triangles[i].vertexIndex[2]];
+			vertex.position.x = ax + (bx - ax) * (p->position.y - ay) / (by - ay);
+			vertex.position.y = p->position.y;
+			TriangleSample(vertices[triangles[i].vertexIndex[0]], *p, vertex);
+			TriangleSample(vertices[triangles[i].vertexIndex[1]], *p, vertex);
+		}
+		else
+		{
+			if (ay == by)
+			{
+				TriangleSample(
+					vertices[triangles[i].vertexIndex[2]],
+					vertices[triangles[i].vertexIndex[1]],
+					vertices[triangles[i].vertexIndex[0]]
+					);
+			}
+			else if (ay == cy)
+			{
+				TriangleSample(
+					vertices[triangles[i].vertexIndex[1]],
+					vertices[triangles[i].vertexIndex[0]],
+					vertices[triangles[i].vertexIndex[2]]
+				);
+			}
+			else if (by == cy)
+			{
+				TriangleSample(
+					vertices[triangles[i].vertexIndex[0]],
+					vertices[triangles[i].vertexIndex[1]],
+					vertices[triangles[i].vertexIndex[2]]
+				);
+			}
+			else
+			{
+
+			}
+		}
+	}
+}
+
+
+
 int main()
 {
 	RTWindow window(800, 500, "RTRender");
